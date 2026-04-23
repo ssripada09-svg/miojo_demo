d7de15cde05b6204485d0503f96cc3d01b05f313
Shashank Sripada <ssripada09@gmail.com>
2026-04-23 19:32:36 +0000

    fix: use transparent black pharos mark and enlarge logo

--- components/navigation/sidebar.tsx
+++ components/navigation/sidebar.tsx
@@ -58,11 +58,11 @@ export function Sidebar({ onNavigate }: SidebarProps) {
         <Link href="/" className="flex items-center gap-3 min-w-0" onClick={handleNavClick}>
           <div className="w-10 h-10 flex items-center justify-center flex-shrink-0">
             <Image
-              src="/brand/pharos-mark-black.jpg"
+              src="/brand/pharos-mark-black.png"
               alt="Pharos"
-              width={40}
-              height={40}
-              className="w-9 h-9 object-contain"
+              width={52}
+              height={52}
+              className="w-11 h-11 object-contain"
               priority
             />
           </div>
